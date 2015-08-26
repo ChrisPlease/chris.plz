0a9f437f783f8d8611b69454fb2f76a275e23bd5
ChrisPlease <chris@chrisplease.me>
2015-08-25 21:42:13 -0600

    Nav Menu Styles
    Finally got around to building out the push menu..still needs tweaks,
    but looks good

--- app/scripts/directives/nav.js
+++ app/scripts/directives/nav.js
@@ -5,18 +5,28 @@ app.directive('cpPrimaryNav', ['$document', 'Nav', function($document, Nav) {
 	return {		
 		restrict: 'E',
 		templateUrl: 'views/templates/nav.html',
+		replace: true,
 		link: function(scope, elem, attrs) {
 			var btn, body;
 
-			elem.before('<span class="menu-toggle">Menu</span>');
+			console.log(elem);
+
+			elem.prepend('<span class="menu-toggle"><span></span><span></span><span></span><span class="sr-only">Toggle Menu</span></span>');
 			btn  = angular.element('.menu-toggle');
-			body = angular.element('body').addClass('cp-menu-push');
+			body = angular.element('body');
 
-			// console.log(btn);
+			$document.mouseup(function(e) {
+				if(!btn.is(e.target) && btn.has(e.target).length === 0 && btn.hasClass('active')) {
+					btn.removeClass('active');
+					body.removeClass('menu-open');
+					elem.removeClass('active');
+				}
+			});
 
-			btn.click(function() {
+			btn.on('click', function(e) {
+				btn.toggleClass('active');
 				body.toggleClass('menu-open');
-				return Nav.push(elem, btn.selector);
+				elem.toggleClass('active');
 			});
 		}
 	};
--- app/scripts/services/nav.js
+++ app/scripts/services/nav.js
@@ -4,9 +4,8 @@ app.factory('Nav', function() {
 	var Nav = {
 		push: function(menu, btn) {
 
-			console.log(btn);
-
 			return menu.toggleClass('cp-menu-open');
+			
 		}
 	};
 
